ac81ce5d3f3a7b9308218b79397a7e85c8852404
TemiArtemi <ts.suarez2006@gmail.com>
2026-02-19 20:10:57 +0000

    Añade un nuevo fichero utils.py con dos funciones básicas para cargar el diccionario y obtener la jerarquía de los inputs

---
+++ app/utils.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import json
+import pandas as pd
+import geopandas as gpd
+
+@st.cache_data
+def cargar_configuracion():
+    """Carga el diccionario en caché para no leer el JSON constantemente."""
+    with open("data-proccesed/diccionario_config.json", "r", encoding="utf-8") as f:
+        diccionario = json.load(f)
+    return diccionario
+
+@st.cache_data
+def obtener_jerarquia_categorias(diccionario_config):
+    """
+    Toma el diccionario plano y lo organiza en: 
+    Macro Categoría -> Grupo Slider -> Lista de Actividades
+    """
+    categorias_agrupadas = {}
+    
+    for actividad, valores in diccionario_config.items():
+        macro = valores["macro_categoria"]
+        grupo = valores["grupo_slider"]
+        
+        if macro not in categorias_agrupadas:
+            categorias_agrupadas[macro] = {}
+            
+        if grupo not in categorias_agrupadas[macro]:
+            categorias_agrupadas[macro][grupo] = []
+            
+        categorias_agrupadas[macro][grupo].append(actividad)
+        
+    return categorias_agrupadas
